e665bd9dc6e9125fd6caaab5f81070b3726d3a33
Unknown <mirkobozic90@gmail.com>
2017-09-28 12:09:27 -0400

    radio buttons/radiogroup
    added two radio buttons inside a radio group and adjusted layout for landscape view

--- MBProject/app/src/main/java/ca/on/conestogac/mbproject/HomePage.java
+++ MBProject/app/src/main/java/ca/on/conestogac/mbproject/HomePage.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -17,41 +18,52 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
     }
 
-        public void playMode(View view)
-    {
+    public void numberOfPlayers(View view) {
         ToggleButton playerToggle = (ToggleButton) findViewById(R.id.playersToggle);
-        if(playerToggle.isChecked())
-                {
-                    SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
-                    playersBar.setVisibility(View.VISIBLE);
-                    playersBar.setProgress(0);
+        if (playerToggle.isChecked()) {
+            SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
+            playersBar.setVisibility(View.VISIBLE);
+            playersBar.setProgress(0);
 
-                    final TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
+            final TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
 
-                    playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-                    {
-                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-                        {
-                            progress = progress + 2;
-                            playersNumber.setText(String.valueOf(progress));
-                        }
-                        public void onStartTrackingTouch(SeekBar seekBar)
-                        {
-
-                        }
-                        public void onStopTrackingTouch(SeekBar seekBar)
-                        {
-
-                        }
-                    });
+            playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    progress = progress + 2;
+                    playersNumber.setText(String.valueOf(progress));
                 }
-                else
-                {
-                    SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
-                    playersBar.setVisibility(View.INVISIBLE);
 
-                    TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
-                    playersNumber.setText("2");
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
                 }
+
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        } else {
+            SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
+            playersBar.setVisibility(View.INVISIBLE);
+
+            TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
+            playersNumber.setText("2");
+        }
+    }
+
+    public void playMode(View view){
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioStandard:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radioCommander:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
