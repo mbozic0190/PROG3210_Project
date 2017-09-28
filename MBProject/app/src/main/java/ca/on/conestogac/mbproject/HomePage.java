0d0be1a46b831dff060d6abfa6bb3e0077afe745
Unknown <mirkobozic90@gmail.com>
2017-09-28 10:16:06 -0400

    Seekbar visibility
    added feature to make seekbar visible or invisible depending on the playmode toggle

--- MBProject/app/src/main/java/ca/on/conestogac/mbproject/HomePage.java
+++ MBProject/app/src/main/java/ca/on/conestogac/mbproject/HomePage.java
@@ -2,35 +2,56 @@ package ca.on.conestogac.mbproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class HomePage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
 
+        public void playMode(View view)
+    {
+        ToggleButton playerToggle = (ToggleButton) findViewById(R.id.playersToggle);
+        if(playerToggle.isChecked())
+                {
+                    SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
+                    playersBar.setVisibility(View.VISIBLE);
+                    playersBar.setProgress(0);
 
-        SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
-        final TextView playersNumber = (TextView)findViewById(R.id.playersNumber);
+                    final TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
 
-        playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
-        {
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-            {
-                playersNumber.setText(String.valueOf(progress));
-            }
+                    playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+                    {
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+                        {
+                            progress = progress + 2;
+                            playersNumber.setText(String.valueOf(progress));
+                        }
+                        public void onStartTrackingTouch(SeekBar seekBar)
+                        {
 
-            public void onStartTrackingTouch(SeekBar seekBar) {
+                        }
+                        public void onStopTrackingTouch(SeekBar seekBar)
+                        {
 
-            }
+                        }
+                    });
+                }
+                else
+                {
+                    SeekBar playersBar = (SeekBar) findViewById(R.id.playersBar);
+                    playersBar.setVisibility(View.INVISIBLE);
 
-            public void onStopTrackingTouch(SeekBar seekBar)
-            {
-
-            }
-        });
+                    TextView playersNumber = (TextView) findViewById(R.id.playersNumber);
+                    playersNumber.setText("2");
+                }
     }
 }
