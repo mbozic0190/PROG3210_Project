e5cf6cf93135552b919b359388ffbe71bfcff710
Unknown <mirkobozic90@gmail.com>
2017-11-02 16:53:20 -0400

    database
    started databasse creation and integration with login screen

--- MBProject/app/src/main/java/ca/on/conestogac/mbproject/GameSetupScreen.java
+++ MBProject/app/src/main/java/ca/on/conestogac/mbproject/GameSetupScreen.java
@@ -3,14 +3,13 @@ package ca.on.conestogac.mbproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class HomePage extends AppCompatActivity {
+public class GameSetupScreen extends AppCompatActivity {
 
     private ToggleButton playerToggle;
     private SeekBar playersBar;
@@ -58,10 +57,25 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_game_setup_screen);
         playerToggle = (ToggleButton) findViewById(R.id.playersToggle);
         playersBar = (SeekBar) findViewById(R.id.playersBar);
         playersNumber = (TextView) findViewById(R.id.playersNumber);
+
+        playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = progress + 2;
+                playersNumber.setText(String.valueOf(progress));
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         }
 
     public void numberOfPlayers(View view) {
@@ -70,20 +84,7 @@ public class HomePage extends AppCompatActivity {
 
             playersBar.setVisibility(View.VISIBLE);
             playersBar.setProgress(0);
-            playersBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    progress = progress + 2;
-                    playersNumber.setText(String.valueOf(progress));
-                }
 
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
         } else {
             playersBar.setVisibility(View.INVISIBLE);
             playersNumber.setText("2");
--- MBProject/app/src/main/java/ca/on/conestogac/mbproject/LoginScreen.java
+++ MBProject/app/src/main/java/ca/on/conestogac/mbproject/LoginScreen.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import ca.on.conestogac.mbproject.LoginRestister.sql.DatabaseHelper;
 
 
 public class LoginScreen extends AppCompatActivity {
+
+    private final AppCompatActivity activity = LoginScreen.this;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        databaseHelper = new DatabaseHelper(activity);
     }
 
 
@@ -23,10 +28,31 @@ public class LoginScreen extends AppCompatActivity {
         EditText password = (EditText) findViewById(R.id.password);
         userName.requestFocus();
 
-        if (userName.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+
+        if (databaseHelper.checkUser(userName.getText().toString().trim()
+                , password.getText().toString().trim())) {
+
+
             Toast.makeText(this, "Login Successful, welcome",
                     Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, HomePage.class);
+            Intent accountsIntent = new Intent(this, GameSetupScreen.class);
+            startActivity(accountsIntent);
+        }
+        else
+        {
+            AlertDialog.Builder loginError  = new AlertDialog.Builder(this);
+            loginError.setMessage("Wrong username or password. Please try again");
+            loginError.setTitle("Login Attempt");
+            loginError.setPositiveButton("OK", null);
+            loginError.setCancelable(true);
+            loginError.create().show();
+            password.setText("");
+        }
+
+    /*    if (userName.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            Toast.makeText(this, "Login Successful, welcome",
+                    Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, GameSetupScreen.class);
             startActivity(intent);
         } else {
             AlertDialog.Builder loginError  = new AlertDialog.Builder(this);
@@ -37,6 +63,6 @@ public class LoginScreen extends AppCompatActivity {
             loginError.create().show();
             password.setText("");
 
-        }
+        }*/
     }
 }
